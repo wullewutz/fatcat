7699e7e85521e4f913b0e4d4c103d84049cbc204
wullewutz <wullewutz@users.noreply.github.com>
2020-06-11 11:15:46 +0200

    Added data of the 1st birtday
    + Changed switched to english for international spectators
    + Updated names to the current form
    + Added propper timestamps to raw data

--- fatcat.py
+++ fatcat.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('fatcat.dat', index_col=0, sep=' ')
+df = pd.read_csv('fatcat.dat', index_col=0, parse_dates=True, sep=' ')
 fig, axes = plt.subplots(nrows=2, ncols=1)
 
-axes[0].set_title('Gewicht absolut')
+axes[0].set_title('Weight (absolute)')
 axes[0].set_ylabel('g')
 df.plot(ax=axes[0], marker='o', lw=2.0)
 axes[0].set_xlabel('')
 axes[0].grid(True, axis='y')
 
-axes[1].set_title('Gewichtszuwachs')
+axes[1].set_title('Weight increase')
 axes[1].set_ylabel('g/Tag')
 df.diff().plot(ax=axes[1], marker='o', lw=2.0)
 axes[1].set_xlabel('')
 axes[1].grid(True, axis='y')
 
-fig.suptitle('Kalis Kätzlich', fontsize = 20, fontweight='bold')
+fig.suptitle("Kali's Kittens", fontsize = 20, fontweight='bold')
 plt.show()
