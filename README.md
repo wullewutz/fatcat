5d5e791b63a18d94f8f4a0bb414642c06eb2a55e
wullewutz <wullewutz@users.noreply.github.com>
2020-08-21 21:51:14 +0200

    Added plots dir and README.md

---
+++ README.md
@@ -0,0 +1,39 @@
+# fatcat - Kalis kitten's weight analysis
+
+## The Story
+
+During early spring 2019 our young cat Kali started behaving strange and was gaining weight quickly.
+The dialog at our vet by that time could be summarized as follows:
+
+Vet: Well, the cat is pregnant.
+We: But she is castrated!
+Vet: Really? That's rare, but happens, if not done properly. Who did the surgery?
+We: It was done here...
+Vet: ...
+
+So, a few weeks later in the heat of summer 2019, four cute kittens were given birth by Kali.
+
+## The Data
+
+Since you should weigh young kittens regularly to check their health and I really love big-fat data,
+I decided to keep the weights of each kitten in a .csv file and analyze and plot it using
+python, pandas and matplotlib. Since this started as a private-only repository, you will find
+some German commit messages among the early commits - sorry!
+
+Also note, that it was not possible to weight every kitten every day. 
+I interpolated those days manually, but unfortunately not marked it in the raw data. Sorry for that as well.
+
+## The Analysis
+
+### First month
+
+![First Month](plots/first_month.png)
+
+### First three months
+
+![First Three Month](plots/first_three_months.png)
+
+### All time
+
+![All time](plots/all_time.png)
+
--- fatcat.py
+++ fatcat.py
@@ -1,7 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import shutil
 
 df = pd.read_csv('fatcat.dat', index_col=0, parse_dates=True, sep=' ')
+
+# Increase figure sizes to 10 x 8 inches
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0] = 10
+fig_size[1] = 8
+plt.rcParams["figure.figsize"] = fig_size
+
+out_path = 'plots'
+if os.path.exists(out_path) and os.path.isdir(out_path):
+    shutil.rmtree(out_path)
+os.mkdir(out_path)
 
 # first month
 fig_1m, axes_1m = plt.subplots(nrows=2, ncols=1)
@@ -16,6 +29,7 @@ df['2019-06-09':'2019-07-08'].diff().plot(ax=axes_1m[1], marker='o', lw=2.0)
 axes_1m[1].set_xlabel('')
 axes_1m[1].grid(True, axis='y')
 fig_1m.suptitle("First month", fontsize = 20, fontweight='bold')
+plt.savefig('plots/first_month.png', dpi=300)
 
 # first three months
 fig_3m, axes_3m = plt.subplots(nrows=2, ncols=1)
@@ -30,6 +44,7 @@ df['2019-06-09':'2019-09-02'].diff().plot(ax=axes_3m[1], marker='o', lw=2.0)
 axes_3m[1].set_xlabel('')
 axes_3m[1].grid(True, axis='y')
 fig_3m.suptitle("First three months", fontsize = 20, fontweight='bold')
+plt.savefig('plots/first_three_months.png', dpi=300)
 
 # all time weight only
 fig_all, axes_all = plt.subplots(nrows=1, ncols=1)
@@ -39,5 +54,6 @@ df.plot(ax=axes_all, marker='o', lw=2.0)
 axes_all.set_xlabel('')
 axes_all.grid(True, axis='y')
 fig_all.suptitle("All time", fontsize = 20, fontweight='bold')
+plt.savefig('plots/all_time.png', dpi=300)
 
 plt.show()
